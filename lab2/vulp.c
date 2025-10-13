3d39874c1ab9226ff1982e30ce480682f06c1410
Niyaz Shakeel (seed) <niyaz@unc.edu>
2025-10-12 20:04:53 -0400

    Completed code for task 3A

--- lab2/vulp.c
+++ lab2/vulp.c
@@ -7,18 +7,37 @@ int main()
     char *fn = "/tmp/XYZ";
     char buffer[60];
     FILE *fp;
+    uid_t ruid = getuid();
+    uid_t euid = geteuid();
 
     /* get user input */
     scanf("%50s", buffer);
 
+    // temporarily disable the root privilage using seteuid
+    if (seteuid(ruid) == -1)
+    {
+        printf("some error happened\n");
+        return 1;
+    }
     if (!access(fn, W_OK))
     {
         // sleep(10); Removing Sleep after completion of Task 2A.
+
+        // Grant back the root privilage using seteuid
+        if (seteuid(euid) == -1)
+        {
+            printf("some error happened\n");
+            return 1;
+        }
         fp = fopen(fn, "a+");
         fwrite("\n", sizeof(char), 1, fp);
         fwrite(buffer, sizeof(char), strlen(buffer), fp);
         fclose(fp);
     }
     else
+    {
+        // always correctly restore privileges before exiting
+        seteuid(euid);
         printf("No permission \n");
+    }
 }
