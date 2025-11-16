b0708cb6cc45ddb51a5a94547baa2047c1930ef5
Niyaz Shakeel (seed) <niyaz@unc.edu>
2025-11-16 09:23:50 -0500

    Completed lab 4 task 1.1B

--- lab4/volumes/task-1.1B.py
+++ lab4/volumes/task-1.1B.py
@@ -8,14 +8,16 @@ def print_pkt(pkt):
 
 ##TODO:
 # - Copy your iface value from running the ifconfig command below.
-
-# - Modify the filter parameter to 
+iface_value = 'br-dc62965e0899'
+# - Modify the filter parameter to
 
 #   (1) Capture any TCP packet the comes from a particular IP and with a destination port number 23.
-
-#   Make sure you take screenshots 
+tcp_filter = 'tcp and src host 10.9.0.1 and dst port 23'
+#   Make sure you take screenshots
+# pkt = sniff(iface=iface_value, filter=tcp_filter, prn=print_pkt)
 
 #   (2) Capture packets that come from or go to the subnet
 #       10.9.0.0/24
 
-pkt = sniff(iface='', filter='', prn=print_pkt)        
+subnet_filter = 'net 10.9.0.0/24'
+pkt = sniff(iface=iface_value, filter=subnet_filter, prn=print_pkt)
